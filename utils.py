add681e0b6c4f5f5d8f20f964ea6f5e40f77e748
efti-nile <efti.nile@gmail.com>
2020-04-30 00:04:23 +0400

    Reid annotation widget now works

--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
+import os
+
+import cv2
+
+
 def create_reid_dict(tblists):
     """
     Args:
@@ -32,3 +37,17 @@ def refine_tracking_box(tb, mask):
     for attr_name in tb.DATA_LISTS:
         attr = getattr(tb, attr_name)
         setattr(tb, attr_name, apply_mask(attr, mask))
+
+
+def save_tblists_for_reid(tblists, dataset_name, datasets_root, id_gen):
+    """Saves a list of lists of `TrackingBox` objects for ReID training.
+    Folder structure:
+    $dataset_root/$dataset_name/$cam_id/$reid_id/*.jpg
+    """
+    for cam_id, tbl in enumerate(tblists):
+        for tb in tbl:
+            if tb.reid_id is not None:
+                dir_path = os.path.join(datasets_root, dataset_name, str(cam_id), str(id_gen.get_id()))
+                os.makedirs(dir_path, exist_ok=True)
+                for img_id, img in enumerate(tb.imgs):
+                    cv2.imwrite(os.path.join(dir_path, f'{img_id}.jpg'), img)
